a6c853b51d5b99499d5b7f1c3563751a5011484d
Asan M <asan_ibrahim@onwardgroup.com>
2026-04-30 21:40:51 +0530

    css changes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Link to="/">MyShop</Link>
+
+      <div style={{ display: "flex", gap: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+      </div>
+
+      <div>Cart (0)</div>
+    </div>
+  );
+}
+
+export default Navbar;
