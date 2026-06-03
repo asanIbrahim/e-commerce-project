d3eabc3a8f5f7efc0394a51694c841ab73d07d4d
Asan M <asan_ibrahim@onwardgroup.com>
2026-06-03 22:46:30 +0530

    add pagination

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
         alignItems: "center",
       }}
     >
-      <Link to="/">MyShop</Link>
+      <Link to="/">MyShops</Link>
 
       <div style={{ display: "flex", gap: "20px" }}>
         <NavLink
