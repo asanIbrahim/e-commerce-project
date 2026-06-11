76618a444bcb9e5793090499ccbb0b608e43b476
Asan M <asan_ibrahim@onwardgroup.com>
2026-06-12 00:41:45 +0530

    remove pagination

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,8 +5,6 @@ function Products() {
   const [data, setData] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
 
   async function fetchData() {
     setLoading(true);
@@ -21,32 +19,18 @@ function Products() {
       if (err instanceof Error) {
         setError(err.message);
       }
-   }
-   finally{
-    setLoading(false)
+    } finally {
+      setLoading(false);
     }
   }
   useEffect(() => {
     fetchData();
   }, []);
 
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentItems = data.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(data.length / itemsPerPage);
-
-  const handlePreviousPage = () => {
-    if (currentPage > 1) setCurrentPage(currentPage - 1);
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
-  };
-
   return (
     <>
-    { loading && <h5>Loading.....</h5>}
-    { error && <h5> {error}</h5> }
+      {loading && <h5>Loading.....</h5>}
+      {error && <h5> {error}</h5>}
       <table border={2} style={{ margin: "50px" }}>
         <thead>
           <tr>
@@ -56,7 +40,7 @@ function Products() {
           </tr>
         </thead>
         <tbody>
-          {currentItems.map((val) => (
+          {data.map((val) => (
             <tr key={val.id}>
               <td>{val.id}</td>
               <td> {val.price}</td>
@@ -65,18 +49,6 @@ function Products() {
           ))}
         </tbody>
       </table>
-
-      <div style={{ margin: "50px", textAlign: "center" }}>
-        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span style={{ margin: "0 15px" }}>
-          Page {currentPage} of {totalPages}
-        </span>
-        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-          Next
-        </button>
-      </div>
     </>
   );
 }
